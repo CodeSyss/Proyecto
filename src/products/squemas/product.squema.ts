f33acffaae9aa121caf310c44b9e1a1e8dd5b347
Carlos Hernández <carlos-hernandez@correo.unimet.edu.ve>
2025-03-23 14:07:15 -0400

    Product created in mongoDB

--- src/products/squemas/product.squema.ts
+++ src/products/squemas/product.squema.ts
@@ -1,37 +1,6 @@
 import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-
-// export class Product {
-
-//     const Product = new mongoose.Schema(
-//         {
-//             name: { 
-//                 type: String, required: true 
-//             },
-
-//             description: { 
-                
-//                 type: String, required: true 
-//             },
-
-//             price: { 
-//                 type: Number, required: true 
-//             },
-
-//             category: { 
-//                 type: String, required: true 
-//             },
-
-//             createdAt: { 
-//                 type: Date, default: Date.now 
-//             },
-
-
-//         }
-//     ) 
-// }
-
 @Schema() 
 export class Product extends Document {
   @Prop({ required: true }) 
@@ -45,9 +14,6 @@ export class Product extends Document {
 
   @Prop({ required: true })
   category: string;
-
-  @Prop({ default: Date.now })
-  createdAt: Date;
 }
 
 export const Productschema = SchemaFactory.createForClass(Product);
